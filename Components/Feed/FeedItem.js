9bc20a5b631d9b30c4f98bbba5adcb4d38ab94fb
hanodims <hend--mohammed@outlook.com>
2020-11-18 20:26:50 +0300

    FeedList & FeedItem

---
+++ Components/Feed/FeedItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//style
+import { Alert, Image } from "react-native";
+import styles from "./styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const FeedItem = ({ post }) => {
+  console.log(`${post.description}`);
+
+  return (
+    <TouchableOpacity
+      onPress={() => Alert.alert("You pressed me", `${post.description}`)}
+    >
+      <Image
+        style={styles.feedListImage}
+        source={require("../../assets/icon.png")}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default FeedItem;
---
+++ Redux/Actions/data.js
@@ -0,0 +1,54 @@
+export const data = [
+  {
+    liked_by: [
+      {
+        user: "Asrar",
+        gender: "Female",
+        image: "profile.png",
+      },
+    ],
+    description: "today's outfit1",
+    created: "2020/11/18",
+    modified: "",
+    owner: [{ user: "Hend", gender: "Female", image: "profile.png" }],
+  },
+  {
+    liked_by: [
+      {
+        user: "Salma",
+        gender: "Female",
+        image: "profile.png",
+      },
+    ],
+    description: "today's outfit2",
+    created: "2020/11/16",
+    modified: "",
+    owner: [{ user: "Asrar", gender: "Female", image: "profile.png" }],
+  },
+  {
+    liked_by: [
+      {
+        user: "Lubna",
+        gender: "Female",
+        image: "profile.png",
+      },
+    ],
+    description: "today's outfit3",
+    created: "2020/11/17",
+    modified: "",
+    owner: [{ user: "Hend", gender: "Female", image: "profile.png" }],
+  },
+  {
+    liked_by: [
+      {
+        user: "Shosho",
+        gender: "Female",
+        image: "profile.png",
+      },
+    ],
+    description: "today's outfit4",
+    created: "2020/11/11",
+    modified: "",
+    owner: [{ user: "Salma", gender: "Female", image: "profile.png" }],
+  },
+];

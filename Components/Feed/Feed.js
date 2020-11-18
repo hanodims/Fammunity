397e140fa184726dd4321ae3425f26aa635193c2
hanodims <hend--mohammed@outlook.com>
2020-11-18 15:17:43 +0300

    fix flatlist

--- Components/Feed/Feed.js
+++ Components/Feed/Feed.js
@@ -2,25 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 
 //style
-import { Container, Text } from "native-base";
+import { Container, Content, Text, View } from "native-base";
 import styles from "./styles";
 import { FlatList } from "react-native-gesture-handler";
 
 const Feed = ({ feeds }) => {
-  console.log(`${feeds}`);
-  function feedsList({ post }) {
-    return <Text>hi</Text>;
+  function feedsList({ item }) {
+    console.log(`${item.description}`);
+    return <Text>{item.description}</Text>;
   }
 
   return (
-    <Container style={styles.FeedDev}>
-      <Text>HI, YOU CAN SEE ME</Text>
-      <FlatList
-        data={feeds}
-        numColumns={3}
-        renderItem={feedsList}
-        keyExtractor={(item, index) => index.toString()}
-      />
+    <Container>
+      <Container style={styles.FeedDev}>
+        <Container></Container>
+        <FlatList
+          data={feeds}
+          numColumns={3}
+          renderItem={feedsList}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </Container>
     </Container>
   );
 };
--- Components/Feed/styles.js
+++ Components/Feed/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export default styles = StyleSheet.create({
   FeedDev: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
